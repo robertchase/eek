678d1f0be2dfea311bafc9dfca7e8e93f62132ec
bob chase <bob.chase@gmail.com>
2020-05-26 10:42:27 -0400

    fancier path derivation

--- eek.sh
+++ eek.sh
@@ -1,7 +1,7 @@
-EPASS=$(openssl enc -nosalt -aes-256-cbc -a -in "$EEK_FILE")
+export EPASS=$(openssl enc -nosalt -aes-256-cbc -a -in "$EEK_FILE")
 alias eek='curl -Ls "$EEK_NET" | openssl enc -pass env:EPASS -d -aes-256-cbc -a | grep'
 alias leek='cat "$EEK_LOCAL" | openssl enc -pass env:EPASS -d -aes-256-cbc -a | grep'
-alias leep='source "$DIR"/leep.sh'
-alias leem='source "$DIR"/leem.sh'
+alias leep='source "${EEK_DIR:-${GIT:-$HOME/git}/eek}/leep.sh"'
+alias leem='source "${EEK_DIR:-${GIT:-$HOME/git}/eek}/leem.sh"'
 alias akk='openssl enc -pass env:EPASS -aes-256-cbc -a -in'
-PS1='eek$ '
+export PS1='eek$ '
